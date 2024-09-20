946e484a649c4a1d7baefc0737c0da0a1456ab5a
anilkumarinjam <injamanilchowdary@gmail.com>
2024-09-19 20:02:29 -0500

    Update App.py

--- App/App.py
+++ App/App.py
@@ -131,7 +131,7 @@ st.set_page_config(
 def run():
     
     # (Logo, Heading, Sidebar etc)
-    img = Image.open('../Logo/RESUM.png')
+    img = Image.open('/Logo/RESUM.png')
     st.image(img)
     st.sidebar.markdown("# Choose Something...")
     activities = ["User", "Feedback", "About", "Admin"]
